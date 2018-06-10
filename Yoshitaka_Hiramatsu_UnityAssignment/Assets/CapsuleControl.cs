cf11cdda6d538b9d08e248e274d7e0f3e17e1ed6
Yoshitaka Hiramatsu <yoshitaka.too@gmail.com>
2018-06-10 23:18:02 +0900

    Coroutine Test done

---
+++ Yoshitaka_Hiramatsu_UnityAssignment/Assets/CapsuleControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("MoveRotateExpand");
+	}
+
+	IEnumerator MoveRotateExpand () {
+		yield return new WaitForSeconds (1.0f);
+		for (int f = 0; f < 100; f++) {
+			transform.position += Vector3.right * 2.0f * Time.deltaTime;
+			yield return null;
+		}
+		for (int f = 0; f < 180; f++) {
+			transform.Rotate (new Vector3 (0, 0, 2));
+			yield return null;
+		}
+		for (int f = 0; f < 25; f++) {
+			transform.localScale += new Vector3 (0.02f, 0.02f, 0.02f);
+			yield return null;
+		}
+	}
+}
