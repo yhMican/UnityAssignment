d5c6b5d94ba6c0f1b85f6ba640ba628a25c26018
Yoshitaka Hiramatsu <yoshitaka.too@gmail.com>
2018-06-10 17:25:50 +0900

    first third of unity assignments

---
+++ Yoshitaka_Hiramatsu_InternshipAssignment_RWG/Assets/CubeControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CubeControl : MonoBehaviour {
+
+	public GameObject CubeInfo;  // Stores Canvas which is a child of the Cube object
+	// Use this for initialization
+	void Start () {
+		CubeInfo.SetActive (false);
+	}
+	public void OnUserAction () {
+		//Debug.Log ("OnUserAction");
+		if (!CubeInfo.activeSelf) {
+			CubeInfo.SetActive (true);
+		} else {
+			CubeInfo.SetActive (false);
+		}
+
+	}
+}
---
+++ Yoshitaka_Hiramatsu_InternshipAssignment_RWG/Assets/TapEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapEffect : MonoBehaviour {
+
+	[SerializeField, Header("タップエフェクト")]
+	private ParticleSystem _tapEffect = null;
+
+	[SerializeField, Header("パーティクル用カメラ")]
+	private Camera _particleCamera = null;
+
+
+	// Update is called once per frame
+	void Update () {
+		// Returns true while left click is being pressed
+		if (Input.GetMouseButtonDown(0)) {
+			_tapEffect.transform.position = _particleCamera.ScreenToWorldPoint (Input.mousePosition + _particleCamera.transform.forward * 10);
+			_tapEffect.Emit (1); // Emits 1 particle immediately
+		}
+	}
+}
